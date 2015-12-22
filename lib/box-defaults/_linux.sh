9be597c5eff660d309c3456586854180da7c4046
Russell Sherman <russell.t.sherman@gmail.com>
2015-12-22 14:24:44 -0600

    more mate keybindings

--- lib/box-defaults/_linux.sh
+++ lib/box-defaults/_linux.sh
@@ -17,5 +17,18 @@ gsettings set org.mate.terminal.keybindings new-tab "<Control>t"
 gsettings set org.mate.terminal.keybindings new-window "<Control>n"
 gsettings set org.mate.terminal.keybindings paste "<Control>v"
 gsettings set org.mate.terminal.keybindings reset-and-clear "<Control>k"
+gsettings set org.mate.Marco.global-keybindings run-command-screenshot "<Primary><Shift>numbersign"
+gsettings set org.mate.Marco.global-keybindings run-command-window-screenshot "<Primary><Shift>dollar"
+gsettings set org.mate.Marco.window-keybindings activate-window-menu "[]"
+gsettings set org.mate.Marco.global-keybindings switch-to-workspace-down ""
+gsettings set org.mate.Marco.global-keybindings switch-to-workspace-up ""
+gsettings set org.mate.Marco.global-keybindings switch-to-workspace-left ""
+gsettings set org.mate.Marco.global-keybindings switch-to-workspace-right ""
+gsettings set org.mate.Marco.window-keybindings move-to-workspace-down ""
+gsettings set org.mate.Marco.window-keybindings move-to-workspace-up ""
+gsettings set org.mate.Marco.window-keybindings move-to-workspace-left ""
+gsettings set org.mate.Marco.window-keybindings move-to-workspace-right ""
+
 ok
 
+# turn on alt-space for Mutate default
