50a05cc00fece77515aac2f2364e4bdd0cff409b
russell sherman <bhedana@gail.com>
2015-12-21 21:17:30 -0600

    clean up after installing atom

--- lib/box-text-editor/atom.sh
+++ lib/box-text-editor/atom.sh
@@ -9,9 +9,10 @@ fi
 
 if [ "$NS_PLATFORM" == "linux" ]; then
   # Currently only a 64-bit version is available.
-  # The Linux version does not currently automatically update 
+  # The Linux version does not currently automatically update
   # so you will need to repeat these steps to upgrade to future releases.
   wget https://github.com/atom/atom/releases/download/v1.3.2/atom-amd64.deb
   sudo dpkg --install atom-amd64.deb
+  rm -f atom-amd64.deb
   # Launch Atom using the installed atom command.
 fi
