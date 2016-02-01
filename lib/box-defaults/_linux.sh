4b615af462fcd9d82a9581e215c818d58974484f
Russell Sherman <russell.t.sherman@gmail.com>
2016-01-31 22:13:33 -0600

    mutate is GUI so it belongs with defaults, not essentials

--- lib/box-defaults/_linux.sh
+++ lib/box-defaults/_linux.sh
@@ -32,3 +32,37 @@ gsettings set org.mate.Marco.window-keybindings move-to-workspace-right ""
 ok
 
 # turn on alt-space for Mutate default
+
+#########################################################
+action "Installing Mutate"
+sudo $pm install -y \
+  qt5-qmake \
+  qt5-default \
+  libgtk2.0-dev \
+  libqt5x11extras5-dev \
+  libboost-regex-dev
+
+git clone https://github.com/qdore/Mutate.git
+cd Mutate/src
+qmake
+make
+
+sudo make install
+cd ..
+sudo cp info/mutate.png /usr/share/icons
+sudo cp info/Mutate.desktop /usr/share/applications
+mkdir -p ~/.config/Mutate
+cp -R config/* ~/.config/Mutate
+chmod -R a+x ~/.config/Mutate/scripts
+chmod -R a+w ~/.config/Mutate
+sed -i "s|{home}|$HOME|g" ~/.config/Mutate/config.ini
+rm -rf Mutate
+ok
+
+action "Adding Mutate to autostart"
+cp /usr/share/applications/Mutate.desktop ~/.config/autostart/mutate.desktop
+ok
+
+action "Change default keybinding for mutate to Alt+Space"
+sed -i -- 's/HotKey=Alt+D/HotKey=Alt+Space/g' ~/.config/Mutate/config.ini
+ok
