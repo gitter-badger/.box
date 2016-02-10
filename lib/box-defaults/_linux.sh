d48a9d0776e492b4c55cad861b630088659e276c
Russell Sherman <russell.t.sherman@gmail.com>
2016-02-10 07:28:48 -0600

    update box-defaults

--- lib/box-defaults/_linux.sh
+++ lib/box-defaults/_linux.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#test if aptitude exists and default to using that if possible
+if command -v aptitude >/dev/null 2>&1 ; then
+  pm="aptitude"
+else
+  pm="apt-get"
+fi
+info -e "Using $pm for package installation\n"
+
 action "Remapping Super_L"
 touch $HOME/.Xmodmap
 profile_write 'clear Mod4' $HOME/.Xmodmap
